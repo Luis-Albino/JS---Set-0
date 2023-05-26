59a9382b1a8343c999aa908d76d49dfc763b0a8b
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-05-25 22:45:31 -0600

    Update C13-2.js

--- C13-2.js
+++ C13-2.js
@@ -1,36 +1,9 @@
-function button () {
-    /// The button ///
-    let btn = document.createElement("button");
-    btn.setAttribute("type","button");
-    btn.style.cursor = "pointer";
-    btn.innerText = "Click me"
-    document.body.appendChild(btn);
-
-    //// CLOSURE parameter /////
-    let meterValue = 0; // Stored at incrementValue() closure
-
-    /// The meter ///
-    let meter = document.createElement("meter");
-    meter.style.position = "relative";
-    meter.style.left = "10px";
-    meter.setAttribute("value",meterValue);
-    meter.setAttribute("min",0);
-    meter.setAttribute("max",3);
-    meter.setAttribute("high",2);
-    document.body.appendChild(meter);
-
-    function incrementValue () {
-        meterValue++;
-        meter.setAttribute("value",meterValue);
-        if (meterValue==3) {btn.disabled = true;
-            btn.style.cursor = "default";};
-    };
-
-    btn.addEventListener("click",incrementValue);
-
-    document.body.appendChild(document.createElement("br"));
+function incrementValue () {
+    let mtr = document.getElementById("mtr");
+    mtr.value += 1;
+    if (mtr.value === 3) {
+        let btn = document.getElementById("btn");
+        btn.disabled = true;
+        btn.style.cursor = "default";
+    }
 };
-
-button();
-button();
-button();
