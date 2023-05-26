f31e86b5d91045541cacc7c2cdba5eae3ac8fd6a
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-05-25 22:35:55 -0600

    Update C4-6.js

--- C4-6.js
+++ C4-6.js
@@ -1,74 +1,53 @@
 function distance () {
+    let j = arguments.length/2; // Auxiliar length
+    let validInput = true; // Auxiliar boolean variable
+    let dimension;
+    let sum;
 
-    let bool; // Auxiliar boolean variable
-    let vec = []; // vector object
-    let j; // Auxiliar for vector-array dimension
-
-    function vector (arg) {
-        let vecArray = [0,0,0]; // default vector value
-        for (let i=0; i < arg.length; i++) {
-            vecArray[i] = arg[i];
-        }
-        return vecArray;
-    };
-
-    function booleanComparation (arg) {
-        for (let i=1; i < arg.length; i++) {
-            bool = (bool && (typeof arg[i-1] === typeof arg[i]));
-        }
-    };
-
-    function arrayComparation (arg) {
-        j = arg[0].length;
-        bool = (arg.length === 2 && arg[1].length === arg[0].length && (j === 2 || j === 3)) ? true : false;
-        booleanComparation(arg);
-    }
-
-    if (typeof arguments[0] === "number") {
-        j = arguments.length/2;
-        bool = (j === 2 || j === 3) ? true : false;
-        if (bool === false && j < 3) {
-            return "Insufficient parameters"
+    if (j != 1) {
+        for (let el of arguments) {
+            validInput = validInput && (typeof el === "number")
         };
-        booleanComparation(arguments);
-        if (bool) {
-            let vec0 = []; // Auxiliar for vector
-            let vec1 = []; // Auxiliar for vector
-            for (let i=0; i < j; i++) {
-                vec0[i] = arguments[i];
-                vec1[i] = arguments[i+j];
-            };
-            vec[0] = vector(vec0);
-            vec[1] = vector(vec1);
+
+        try {
+            if (j != 1 && j != 2 && j != 3 && j < 3)
+            throw "Insufficient parameters";
+            if (!validInput || j > 3)
+            throw "incompatible point data";
+        }
+        catch (err) {
+            console.log(err)
+            return
+        };
+
+        sum = 0;
+        for (let i=0; i<j; i++) {
+            let delta = arguments[i] - arguments[i+j];
+            sum = sum + delta*delta;
         }
     }
-
-    else if (typeof arguments[0] === "object") {
-        arrayComparation(arguments);
-
-        if (bool) {
-            let bool2 = true;
-            for (let el in arguments) {
-                bool2 = bool && bool2;
-                vec[el] = vector(arguments[el]);
-            }
-            bool = bool2;
+    else {
+        dimension = arguments.length;
+        try {
+            if (dimension != 2 && dimension != 3 || arguments[1].length != dimension)
+            throw "incompatible point data";
         }
-    }
+        catch (err) {
+            console.log(err)
+            return
+        };
+        sum = 0;
+        for (let i=0; i<dimension; i++) {
+            let delta = arguments[1][i] - arguments[0][i];
+            sum = sum + delta*delta;
+        }
 
-    if (!!bool == false) {
-        return "incompatible point data"
-    }
-
-    let sum = 0;
-    for (let i=0; i<j; i++) {
-        let delta = vec[1][i] - vec[0][i];
-        sum = sum + delta*delta;
     }
 
     return Math.sqrt(sum);
 }
 
+
 console.log("distance(1,2,2,2)=",distance(1,2,2,2)); // returns 1 (done as part of exercise 5)
 console.log("distance([1,2],[2,2])=",distance([1,2],[2,2])); // returns 1
-console.log("distance([1,2],[2,2,4])=",distance([1,2],[2,2,4])); // error: incompatible point data
+distance([1,2],[2,2,4]); // error: incompatible point data
