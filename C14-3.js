17ab32b53e488f80f353d8b7fa27c5e36e0687fb
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-05-25 22:49:58 -0600

    Update C14-3.js

--- C14-3.js
+++ C14-3.js
@@ -1,134 +1,107 @@
-import myJson from "./C14-3.json" assert {type: "json"};
+///////////////////////
+//// Home template ////
+///////////////////////
 
+function homeTemplate () {
+    // hide button //
+    document.getElementById("btn").style.display = "none";
 
-/////////////////////////
-// template's background //
-/////////////////////////
-
-function newTemplate (tempName) {
-    let template = document.createElement("OL");
-    template.id = tempName;
-    template.className = tempName;
-    document.body.appendChild(template);
-
-    return template
-}
-
-
-////////////////////////
-////// TEMPLATE 1 //////
-////////////////////////
-
-function templateType1 () {
-
-    let template = newTemplate("template");
-    let fragment = document.createDocumentFragment(); // FRAGMENT
-
-    for (let data in myJson) {
-        let li = document.createElement("LI");
-
-        let img = document.createElement("IMG");
-        img.setAttribute("src",myJson[data].image);
-
-        let dataDiv = document.createElement("DIV");
-        dataDiv.className = "data";
-
-        let title = document.createElement("H2");
-        title.className = "title";
-        title.innerHTML = myJson[data].title;
-
-        let info = document.createElement("P");
-        info.className = "info";
-        info.innerHTML = myJson[data].info;
-
-        dataDiv.appendChild(title);
-        dataDiv.appendChild(info);
-        li.appendChild(img);
-        li.appendChild(dataDiv);
-        // template.appendChild(li); // instead of fragments
-        fragment.appendChild(li); // FRAGMENT
-
-        // add event listeners //
-        img.addEventListener("click",function () { open(data) });
-        title.addEventListener("click",function () { open(data) });
-        info.addEventListener("click",function () { open(data) });
+    // Fill container //
+    let container = document.getElementById("container");
+    while (container.children[0]) {
+        container.children[0].remove();
     };
-    template.appendChild(fragment); // FRAGMENT
+
+    fetch('./C14-3.json').then(function (res) {
+        return res.json();
+    }).then(function (arr) {
+        let i = 1;
+        for (let obj of arr) {
+            let node = document.getElementById("template1").content.cloneNode(true);
+            // children //
+            let section = node.children[0];
+            let number = section.children[0];
+            let image = section.children[1];
+            let title = section.children[2].children[0];
+            let info = section.children[2].children[1];
+            // set HTML //
+            number.innerHTML = i;
+            image.src = obj["image"];
+            title.innerHTML = obj["title"];
+            info.innerHTML = obj["info"];
+            i++;
+            // Events //
+            let index = arr.indexOf(obj)
+            image.addEventListener("click",function () {openURL(index)})
+            title.addEventListener("click",function () {openURL(index)})
+            info.addEventListener("click",function () {openURL(index)})
+            // Append node //
+            container.appendChild(section);
+        }
+    }).catch(function (err){
+        console.log(err)
+    });
 };
 
 
-////////////////////////
-////// TEMPLATE 2 //////
-////////////////////////
+///////////////////////
+//// page template ////
+///////////////////////
 
-function templateType2 (data) {
-    let template = newTemplate("template");
-
-    let div = document.createElement("DIV");
-
-    let img = document.createElement("IMG");
-    img.setAttribute("src",myJson[data].image);
-    img.style.float = "left";
-    img.style.margin = "0px 20px 0px 20px";
-
-    let title = document.createElement("H2");
-    title.className = "title";
-    title.innerHTML = myJson[data].title;
-    title.style.margin = "20px";
-
-    let info = document.createElement("P");
-    info.className = "info";
-    info.innerHTML = myJson[data].info;
-    info.style.margin = "20px";
-
-    div.appendChild(img);
-    div.appendChild(title);
-    div.appendChild(info);
-    template.appendChild(div);
+function template (sectionTitle,sectionImg,sectionInfo) {
+    // hide button //
+    document.getElementById("btn").style.display = "block";
+    
+    // Fill container //
+    let container = document.getElementById("container");
+    while (container.children[0]) {
+        container.children[0].remove();
+    };
+    let node = document.getElementById("template2").content.cloneNode(true);
+    // children //
+    let section = node.children[0];
+    let image = section.children[0];
+    let title = section.children[1];
+    let info = section.children[2];
+    // set HTML //
+    image.src = sectionImg;
+    title.innerHTML = sectionTitle;
+    info.innerHTML = sectionInfo;
+    // Apend //
+    container.appendChild(section);
 };
 
 
-////////////////////////
-//// function OPEN /////
-////////////////////////
+///////////////////////
+/////// Open URL //////
+///////////////////////
 
-function open (data) {
-    let win = window.open("http://127.0.0.1:5555/C14-3.html", data, "");
-}
+function openURL (index) {
+    // Set URL //
+    let url = new URLSearchParams(window.location.search);
+    if (index != undefined) {
+        url.set("index",index);
+        window.location.search = url.toString();
+    }
+    else {
+        url.delete("index");
+        window.location.search = url.toString();
+    }
+};
 
-
-////////////////////////
-// Template rendering //
-////////////////////////
-
-
-if (!window.opener) {
-    templateType1();
+// Get URL //
+let url = new URLSearchParams(window.location.search);
+let index = url.get("index");
+if (index) {
+    fetch('./C14-3.json').then(function (res) {
+        return res.json();
+    }).then(function (arr) {
+        let obj = arr[index];
+        template(obj["title"],obj["image"],obj["info"]);
+    }).catch(function (err){
+        console.log(err)
+    });
 }
 else {
-    newButton(window.opener);
-    templateType2 (window.name);
-};
-
-
-////////////////////////
-//////// BUTTON ////////
-////////////////////////
-
-function newButton (windowOpener) {
-    let btn = window.document.createElement("BUTTON");
-    btn.setAttribute("type","button");
-    btn.addEventListener("click",function () {windowOpener.close()});
-
-    let anchor = window.document.createElement("A");
-    anchor.setAttribute("href","http://127.0.0.1:5555/C14-3.html");
-    anchor.innerHTML = "<< go back";
-
-    btn.appendChild(anchor);
-    document.body.appendChild(btn);
-};
-
-
-
-
-
+    homeTemplate();
+}
