0ae2f1333d67d50a1de5148ee9778cb186cab97d
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-05-30 16:00:29 -0600

    Update C4-2.js

--- C4-2.js
+++ C4-2.js
@@ -1,11 +1,20 @@
-function addRec (array) {
-    let sum = 0;
-    for (let el of array) {
-        sum += el
+function addRec (array,fromIndex = 0) {
+    let sum = array[fromIndex];
+    let newIndex = fromIndex+1;
+    if (newIndex < array.length) {
+        sum += addRec(array,newIndex)
     }
     return sum
-}
+};
 
 var arr = [ 1, 3, 5, 7];
 
 console.log(arr," yields sumRec =",addRec(arr));
+
+// function addRec (array) {
+//     let sum = 0;
+//     for (let el of array) {
+//         sum += el
+//     }
+//     return sum
+// };
