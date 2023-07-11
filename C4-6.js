2dddd1590d5c07a2259e366eb4ca17e1f50fe8bc
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 12:37:30 -0600

    Update C4-6.js

--- C4-6.js
+++ C4-6.js
@@ -3,31 +3,49 @@ function distance () {
     let validInput = true; // Auxiliar boolean variable
     let dimension;
     let sum;
+    let vector = [[],[]];
 
     if (j != 1) {
+
         for (let el of arguments) {
             validInput = validInput && (typeof el === "number")
-        };
-
-        if (j != 1 && j != 2 && j != 3 && j < 3 || !validInput || j > 3) return undefined;
-
-        sum = 0;
-        for (let i=0; i<j; i++) {
-            let delta = arguments[i] - arguments[i+j];
-            sum = sum + delta*delta;
         }
+
+        if (j != 1 && j != 2 && j != 3 && j < 3 || !validInput || j > 3) return;
+
+        for (let i=0; i<j; i++) {
+            vector[0].push(arguments[i+j]);
+            vector[1].push(arguments[i]);
+        }
+
     }
     else {
+
         dimension = arguments.length;
 
-        if (dimension != 2 && dimension != 3 || arguments[1].length != dimension) return undefined;
+        if (dimension != 2 && dimension != 3 || arguments[1].length != dimension) return;
 
-        sum = 0;
-        for (let i=0; i<dimension; i++) {
-            let delta = arguments[1][i] - arguments[0][i];
-            sum = sum + delta*delta;
+        vector[0] = arguments[0];
+        vector[1] = arguments[1];
+
+    }
+
+    try {
+        for (let i=0; i<2; i++) {
+            for (let el of vector[i]) {
+                // Check for possible NaN
+                if (el !== 0 && !el) throw null
+            }
         }
+    }
+    catch (err) {
+        return err
+    }
 
+    sum = 0;
+    for (let i=0; i<j; i++) {
+        let delta = vector[1][i] - vector[0][i];
+        sum = sum + delta*delta;
     }
 
     return Math.sqrt(sum);
