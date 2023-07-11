1e23f38255a10d3d821da3da319433262e91eab4
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-07-11 12:38:34 -0600

    Update C7-2.js

--- C7-2.js
+++ C7-2.js
@@ -1,6 +1,7 @@
 ///// Convert to brazilian format /////
 
 function brDate (usDate) {
+    let hollidays = BrasilianHollidays();
     let validFormat = true;
     let month, day, year;
 
@@ -56,7 +57,7 @@ function brDate (usDate) {
     let date = day+"/"+month+"/"+year;
     let monthList = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
     let monthName = monthList[Number(month)-1];
-    let celebration = hollidaysBR[monthName][Number(day)];
+    let celebration = hollidays[monthName][Number(day)];
     if (!!celebration) {
         date = date + " (" + celebration + ")";
     };
@@ -66,65 +67,67 @@ function brDate (usDate) {
 
 ///// Brazilian Hollidays ////
 
-let hollidaysBR = {
-    jan : {
-        1 : "Ano Novo",
-        6 : "Dia de Reis",
-        30 : "Dia da Saudade",
-        31 : "Dia Mundial dos Mágicos"
-    },
-    feb : {
-        2 : "Dia de Nossa Senhora dos Navegantes",
-        24 : "Promulgação da primeira Constituição da República do Brasil"
-    },
-    mar : {
-        8 : "Dia Internacional da Mulher",
-        17 : "Dia de São Patrício",
-        25 : "Dia da Constituição"
-    },
-    apr : {
-        13 : "Dia do Hino Nacional Brasileiro",
-        19 : "Dia do Índio",
-        21 : "Dia de Tiradentes",
-        22 : "Descobrimento do Brasil"
-    },
-    may : {
-        1 : "Dia do Trabalhador",
-        5 : "Dia Mundial da Língua Portuguesa",
-        13 : "Abolição da Escravatura"
-    },
-    jun : {
-        5 : "Dia Mundial do Meio Ambiente",
-        12 : "Dia dos Namorados",
-        13 : "Dia de Santo Antônio - Festas Juninas"
-    },
-    jul : {
-        2 :  "Independência da Bahia",
-        26 : "Dia dos Avós"
-    },
-    aug : {
-        9 : "Dia Internacional dos Povos Indígenas",
-        11 : "Dia do Estudante"
-    },
-    sep : {
-        5 : "Dia da Amazônia",
-        7 : "Dia da Independência do Brasil",
-        21 : "Dia da Árvore"
-    },
-    oct : {
-        4 : "Dia da Natureza",
-        12 : "Dia das Crianças"
-    },
-    nov : {
-        14 : "Dia do Bandeirante",
-        15 : "Dia da Proclamação da República",
-        19 : "dia da Bandeira"
-    },
-    dec : {
-        25 : "Natal",
-        31 : "Réveillon, Virada do ano"
+function BrasilianHollidays () {
+    return {
+        jan : {
+            1 : "Ano Novo",
+            6 : "Dia de Reis",
+            30 : "Dia da Saudade",
+            31 : "Dia Mundial dos Mágicos"
+        },
+        feb : {
+            2 : "Dia de Nossa Senhora dos Navegantes",
+            24 : "Promulgação da primeira Constituição da República do Brasil"
+        },
+        mar : {
+            8 : "Dia Internacional da Mulher",
+            17 : "Dia de São Patrício",
+            25 : "Dia da Constituição"
+        },
+        apr : {
+            13 : "Dia do Hino Nacional Brasileiro",
+            19 : "Dia do Índio",
+            21 : "Dia de Tiradentes",
+            22 : "Descobrimento do Brasil"
+        },
+        may : {
+            1 : "Dia do Trabalhador",
+            5 : "Dia Mundial da Língua Portuguesa",
+            13 : "Abolição da Escravatura"
+        },
+        jun : {
+            5 : "Dia Mundial do Meio Ambiente",
+            12 : "Dia dos Namorados",
+            13 : "Dia de Santo Antônio - Festas Juninas"
+        },
+        jul : {
+            2 :  "Independência da Bahia",
+            26 : "Dia dos Avós"
+        },
+        aug : {
+            9 : "Dia Internacional dos Povos Indígenas",
+            11 : "Dia do Estudante"
+        },
+        sep : {
+            5 : "Dia da Amazônia",
+            7 : "Dia da Independência do Brasil",
+            21 : "Dia da Árvore"
+        },
+        oct : {
+            4 : "Dia da Natureza",
+            12 : "Dia das Crianças"
+        },
+        nov : {
+            14 : "Dia do Bandeirante",
+            15 : "Dia da Proclamação da República",
+            19 : "dia da Bandeira"
+        },
+        dec : {
+            25 : "Natal",
+            31 : "Réveillon, Virada do ano"
+        }
     }
-};
+}
 
 
 console.log(brDate("1/1/2023"));
