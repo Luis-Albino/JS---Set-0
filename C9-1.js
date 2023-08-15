68d17e2cab8c0a037dc75db83042b768e6b73b46
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-08-14 18:49:40 -0600

    Update C9-1.js

--- C9-1.js
+++ C9-1.js
@@ -1,15 +1,12 @@
 function dataParse (str) {
-
+    
     str = str.replace(/([^\s,{]+):/g, '"$1":');
     str = str.replace(/(function\s?\(.+\)\s?{[^}]+})/g, '"$1"');
 
     let obj = JSON.parse(str);
     for (let prop in obj) {
-        let fnString = obj[prop].toString();
-        if (fnString.search("function") != -1){
-            let fn = new Function('return ' + fnString)();
-            obj[prop] = fn;
-        }
+        let fn = new Function('return ' + obj[prop])();
+        obj[prop] = fn;
     }
     return obj
 }
